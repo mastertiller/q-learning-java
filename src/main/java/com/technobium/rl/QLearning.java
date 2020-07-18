f319a22868cb25a132e71b7646b9da056cedcc27
mastertiller <weishuitian@gmail.com>
2020-07-18 21:26:11 +0800

    123

--- src/main/java/com/technobium/rl/QLearning.java
+++ src/main/java/com/technobium/rl/QLearning.java
@@ -12,7 +12,7 @@ public class QLearning {
     private final double gamma = 0.9; // Eagerness - 0 looks in the near future, 1 looks in the distant future
 
     private final int mazeWidth = 3;
-    private final int mazeHeight = 3;
+    private final int mazeHeight = 323;
     private final int statesCount = mazeHeight * mazeWidth;
 
     private final int reward = 100;
